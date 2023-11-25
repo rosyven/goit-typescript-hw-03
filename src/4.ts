09485a234059d3ca10321993450ed2b8016dd3c3
rosyven <93422035+rosyven@users.noreply.github.com>
2023-11-25 15:39:21 +0200

    1-4 tasks done

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,49 @@
-const key = new Key();
+class Key {
+  constructor(private signature: number) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  tenants: Person[] = [];
+
+  constructor(public key: Key) {
+    this.door = false;
+  }
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('The door is opened.');
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is opened.');
+    } else {
+      console.log('Invalid key.');
+    }
+  }
+}
+
+const key = new Key(Math.floor(Math.random() * 20));
 
 const house = new MyHouse(key);
 const person = new Person(key);
